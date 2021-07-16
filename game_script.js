0ab045e8153780f4d12ebae16baba7e7e2c8cd88
Alex-1701 <55504833+Alex-1701@users.noreply.github.com>
2021-07-16 11:33:20 +0300

    Initial commit

---
+++ game_script.js
@@ -0,0 +1,67 @@
+var object = {
+	player_position: [0, 0],
+	enemy_position: [5, 5],
+	grid : [
+		[1, 2, 3, 4, 5, 4],
+		[1, 3, 0, 0, 2, 1],
+		[4, 3, 0, 0, 4, 3],
+		[2, 5, 0, 0, 5, 1],
+		[1, 3, 4, 3, 2, 3],
+	]
+};
+
+// Цвета должня быть адаптивными к входящему объекту.
+var COLORS = {
+	0 : 'grey',
+	1 : 'rgb(255, 0, 0)', // red
+	2 : 'orange',
+	3 : 'yellow',
+	4 : 'blue',
+	5 : 'green',
+	6 : 'MediumVioletRed',
+}
+
+function generateGrid(object) {
+	var grid = document.getElementById('grid');
+	var arr = object.grid;
+
+	for (let i = 0; i < arr.length; i++) {
+		var row = document.createElement('tr');
+		for (let j = 0; j < arr[i].length; j++) {
+			var cell = document.createElement('td');
+			cell.setAttribute('onclick', `lol(${arr[i][j]})`);
+			cell.setAttribute('onmouseover', "style.borderColor = 'red'; style.borderWidth = '2px'");
+			cell.setAttribute('onmouseout', `style.borderColor = 'black'; style.borderWidth = '2px'`);
+			cell.style.backgroundColor = COLORS[arr[i][j]];
+			row.appendChild(cell);
+		}
+		grid.appendChild(row);
+	}
+}
+
+function lol(color_number) {
+	var example = document.getElementById('example');
+	example.style.backgroundColor = COLORS[color_number];
+	console.log(example.innerText);
+}
+
+function build_select_panel() {
+	console.log('kek');
+	var panel = document.getElementById('select_panel');
+	var table = document.createElement('table');
+	var row = document.createElement('tr');
+
+	Object.keys(COLORS).forEach(e => {
+		var cell = document.createElement('td');
+		console.log(e + "\t" + COLORS[e]);
+		cell.style.backgroundColor = COLORS[e];
+		row.appendChild(cell);
+	});
+
+	table.appendChild(row);
+	panel.appendChild(table);
+}
+
+generateGrid(object);
+build_select_panel();
+
