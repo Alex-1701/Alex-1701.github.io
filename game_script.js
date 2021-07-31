884df2e73263fa287aa826610074d970d7e37028
Alex-1701 <55504833+Alex-1701@users.noreply.github.com>
2021-08-01 00:24:11 +0300

    minimum

--- game_script.js
+++ game_script.js
@@ -2,117 +2,123 @@
 
 var table = null;
 
+var OWNERS = {
+  0 : 'none',
+  1 : 'player',
+  2 : 'enemy',
+  3 : 'free'
+}
+
 function getGridObject() {
-	// Функция должна отдавать готовую играбельную таблицу.
-	var cell = {
-		color: 1,
-		owner: 0,
-	}
+  // Функция должна отдавать готовую играбельную таблицу.
 
-	console.log(cell);
+  //[color,owner]
+  var myMatrix = 
+  [
+    /*
+    [[1,1], [6,3], [3,3], [3,3], [5,3], [4,3]],
+    [[2,3], [1,1], [5,3], [6,3], [3,3], [4,3]],
+    [[4,3], [4,3], [2,3], [5,3], [1,3], [4,3]],
+    [[2,3], [4,3], [4,3], [4,3], [4,3], [4,3]],
+    [[1,3], [3,3], [4,3], [3,3], [4,3], [4,3]],
+    */
+    [[1,1], [1,1], [3,3], [3,3], [5,3], [4,3]],
+    [[1,1], [1,1], [0,0], [0,0], [3,3], [1,3]],
+    [[4,3], [3,3], [0,0], [0,0], [1,3], [3,3]],
+    [[2,3], [5,3], [0,0], [0,0], [4,2], [4,2]],
+    [[1,3], [3,3], [4,3], [3,3], [4,2], [4,2]],
+  ];
 
-	var myMatrix = 
-	[
-		[1, 1, 3, 3, 5, 4],
-		[1, 1, 0, 0, 3, 1],
-		[4, 3, 0, 0, 1, 3],
-		[2, 5, 0, 0, 4, 4],
-		[1, 3, 4, 3, 4, 4],
-	];
+  var randX = 10; 
+  var randY = 10;
+  var randMatrix = new Array(randX);
+  for (var i = 0; i < randX; i++) {
+    randMatrix[i] = new Array(randY);
+    for (var j = 0; j < randY; j++) {
+      //randMatrix[i][j] = Math.floor(Math.random() * (7-1) + 1);
+      randMatrix[i][j] = [1, 0];
+    }
+  }
 
-	var randX = 10; 
-	var randY = 10;
-	var randMatrix = new Array(randX);
-	for (var i = 0; i < randX; i++) {
-		randMatrix[i] = new Array(randY);
-		for (var j = 0; j < randY; j++) {
-			randMatrix[i][j] = Math.floor(Math.random() * (7-1) + 1);
-		}
-	}
+  //console.log(randMatrix);
+  //console.log(JSON.stringify(randMatrix));
 
-	var object = {
-		// Матрица цветов клеток.
-		grid: myMatrix,
-		// Массив клеток принадлежащих игроку.
-		player_array: [[0, 0], [0, 1], [1, 0], [1, 1]],
-		player_color: 1,
-		// Массив клеток принадлежащих противнику.
-		enemy_array: [[4, 4], [3, 5], [3, 4], [4, 5]],
-		enemy_color: 2
-	};
+  var object = {
+    // Матрица цветов клеток.
+    matrix: myMatrix,
+    // Массив клеток принадлежащих игроку.
+    //player_array: [[0, 0], [0, 1], [1, 0], [1, 1]],
+    //player_color: 1,
+    // Массив клеток принадлежащих противнику.
+    //enemy_array: [[4, 4], [3, 5], [3, 4], [4, 5]],
+    //enemy_color: 2
+  };
 
-	return object;
+  return object;
 }
 
 // Цвета должны быть адаптивными к входящему объекту.
 var COLORS = {
-	0 : 'grey',
-	1 : 'rgb(255, 0, 0)', // red
-	2 : 'orange',
-	3 : 'yellow',
-	4 : 'blue',
-	5 : 'green',
-	6 : 'MediumVioletRed',
-}
-
-var OWNERS = {
-	0 : 'free',
-	1 : 'player',
-	2 : 'enemy',
-	3 : 'none'
+  0 : 'grey',
+  1 : 'rgb(255, 0, 0)', // red
+  2 : 'orange',
+  3 : 'yellow',
+  4 : 'blue',
+  5 : 'green',
+  6 : 'MediumVioletRed',
 }
 
 function showColor(color_number) {
-	var example = document.getElementById('example');
-	example.style.backgroundColor = COLORS[color_number];
-	console.log(example.innerText);
+  var example = document.getElementById('example');
+  example.style.backgroundColor = COLORS[color_number];
+  console.log(example.innerText);
 }
 
 function test(event) {
-	console.log(event);
+  console.log(event);
 }
 
 function userTurn(x, y, color) {
-	console.log('Ход игрока');
-	//var finished = false;
-	if(table.changeAreaColor(x, y, 'player')) {
-		console.log('Игрок походил');
-		enemyTurn();
-	}
-	else {
-		console.log('низя так ходить');
-	}
+  console.log('Ход игрока');
+  //var finished = false;
+  if(table.changeAreaColor(x, y, 'player')) {
+    console.log('Игрок походил');
+    enemyTurn();
+  }
+  else {
+    console.log('низя так ходить');
+  }
 }
 
 function enemyTurn() {
-	console.log('Ход противника...');
-	//console.log(Enemy.turn(table.getTableData()));
-	//table.changeAreaColor(x, y, 'enemy');
-	console.log('Противник походил');
+  console.log('Ход противника...');
+  //console.log(Enemy.turn(table.getTableData()));
+  //table.changeAreaColor(x, y, 'enemy');
+  console.log('Противник походил');
 }
 
 function sleep(ms) {
-	// Нужна асинхронность!
-	/*ms += new Date().getTime();
-	while (new Date() < ms){}*/
-	setTimeout(() => console.log('f'), ms);
+  // Нужна асинхронность!
+  /*ms += new Date().getTime();
+  while (new Date() < ms){}*/
+  setTimeout(() => console.log('f'), ms);
 } 
 
 function main() {
-	// Создаём объект класса таблица таблица.
-	table = new Table(getGridObject(), 'grid');
-	
-	// Вставляем табицу в страницу.
-	table.generateTable();
+  // Создаём объект класса таблица таблица.
+  table = new Table(getGridObject(), 'grid');
+  
+  // Вставляем табицу в страницу.
+  table.generateTable();
 
-	while (false) {
-		console.log('Ход игрока');
-		
-	}
-	
-	//build_select_panel();
-	
-	//console.log(t.data_object);
+  while (false) {
+    console.log('Ход игрока');
+    
+  }
+  
+  //build_select_panel();
+  
+  //console.log(t.data_object);
 }
 
 main();
@@ -120,38 +126,38 @@ main();
 
 /*
 function generateGrid(object) {
-	var grid = document.getElementById('grid');
-	var arr = object.grid;
+  var grid = document.getElementById('grid');
+  var arr = object.grid;
 
-	for (let i = 0; i < arr.length; i++) {
-		var row = document.createElement('tr');
-		for (let j = 0; j < arr[i].length; j++) {
-			var cell = document.createElement('td');
-			cell.setAttribute('onclick', `lol(${arr[i][j]})`);
-			cell.setAttribute('onmouseover', "style.borderColor = 'red'; style.borderWidth = '2px'");
-			cell.setAttribute('onmouseout', `style.borderColor = 'black'; style.borderWidth = '2px'`);
-			cell.style.backgroundColor = COLORS[arr[i][j]];
-			row.appendChild(cell);
-		}
-		grid.appendChild(row);
-	}
+  for (let i = 0; i < arr.length; i++) {
+    var row = document.createElement('tr');
+    for (let j = 0; j < arr[i].length; j++) {
+      var cell = document.createElement('td');
+      cell.setAttribute('onclick', `lol(${arr[i][j]})`);
+      cell.setAttribute('onmouseover', "style.borderColor = 'red'; style.borderWidth = '2px'");
+      cell.setAttribute('onmouseout', `style.borderColor = 'black'; style.borderWidth = '2px'`);
+      cell.style.backgroundColor = COLORS[arr[i][j]];
+      row.appendChild(cell);
+    }
+    grid.appendChild(row);
+  }
 }
 
 
 function build_select_panel() {
-	console.log('kek');
-	var panel = document.getElementById('select_panel');
-	var table = document.createElement('table');
-	var row = document.createElement('tr');
-	
-	Object.keys(COLORS).forEach(e => {
-		var cell = document.createElement('td');
-		console.log(e + "\t" + COLORS[e]);
-		cell.style.backgroundColor = COLORS[e];
-		row.appendChild(cell);
-	});
-	
-	table.appendChild(row);
-	panel.appendChild(table);
+  console.log('kek');
+  var panel = document.getElementById('select_panel');
+  var table = document.createElement('table');
+  var row = document.createElement('tr');
+  
+  Object.keys(COLORS).forEach(e => {
+    var cell = document.createElement('td');
+    console.log(e + "\t" + COLORS[e]);
+    cell.style.backgroundColor = COLORS[e];
+    row.appendChild(cell);
+  });
+  
+  table.appendChild(row);
+  panel.appendChild(table);
 }
 */
--- table_class.js
+++ table_class.js
@@ -1,167 +1,301 @@
 'use strict';
 
 class Table {
-	DOM_table_id;
-	grid;
-	player_area;
-	enemy_area;
-	player_color;
-	enemy_color;
+  DOM_table_id;
+  grid;
+  player_color;
+  enemy_color;
+  player_cells_counter;
+  enemy_cells_counter;
+  /*player_area;
+  enemy_area;*/
 
-	constructor(object, table_id) {
-		console.log('Инициализация объекта таблицы');
+  constructor(object, table_id) {
+    console.log('Инициализация объекта таблицы');
 
-		this.DOM_table_id = table_id;
-		this.grid = object.grid;
-		this.player_area = object.player_array;
-		this.enemy_area = object.enemy_array;
-		this.player_color = object.player_color;
-		this.enemy_color = object.enemy_color;
-	}
+    this.DOM_table_id = table_id;
+    this.grid = object.matrix;
 
-	generateTable() {
-		// Метод собирает и вставляем DOM таблицу.
-		var DOM_table = document.getElementById(this.DOM_table_id);
-		var arr = this.grid;
-		
-		for (let i = 0; i < arr.length; i++) {
-			var row = document.createElement('tr');
-			for (let j = 0; j < arr[i].length; j++) {
-				var cell = document.createElement('td');
-				cell.setAttribute('data-x', i);
-				cell.setAttribute('data-y', j);
-				cell.setAttribute('data-cl', arr[i][j]);
-				
-				if(arr[i][j] == 0) {
-					// Если это не играбельная клетка.
-					cell.className = 'neutral';					
-				} else {
-					// Если это обычная клетка.
-					// Привязываем вызов функции к клетке.
-					cell.setAttribute('onclick', 'userTurn(this.dataset.x, this.dataset.y, this.dataset.cl)');
-					// Раскрашиваем клетку.
-					cell.style.backgroundColor = COLORS[arr[i][j]];
-					// По умолчанию клетка свободная.
-					cell.className = 'free';
+    this.grid.forEach(line => {
+      line.forEach(cell => {
+        if (cell[1] == 1) {
+          this.player_color = cell[0];
+          this.player_cells_counter++;
+        }
+        if (cell[1] == 2) {
+          this.enemy_color = cell[0];
+          this.enemy_cells_counter++;
+        }
+      });
+    });
 
-					// Выделение клеток из областей игрока.
-					this.player_area.forEach(element => {
-						if (element[0] == i && element[1] == j){
-							cell.className = 'player';
-						}
-					});
+    //this.player_area = object.player_array;
+    //this.enemy_area = object.enemy_array;
+    //this.player_color = object.player_color;
+    //this.enemy_color = object.enemy_color;
+  }
 
-					// Выделение клеток из областей противника.
-					this.enemy_area.forEach(element => {
-						if (element[0] == i && element[1] == j){
-							cell.className = 'enemy';
-						}
-					});
-				}
+  generateTable() {
+    // Метод собирает и вставляем DOM таблицу.
+    var DOM_table = document.getElementById(this.DOM_table_id);
+    
+    //Удаляем всех потомков, на случай если они уже были.
+    this.removeAllChildNodes(DOM_table);
+    var matrix = this.grid;
+    
+    for (let i = 0; i < matrix.length; i++) {
+      var row = document.createElement('tr');
+      for (let j = 0; j < matrix[i].length; j++) {
+        var cell = document.createElement('td');
+        cell.setAttribute('data-x', i);
+        cell.setAttribute('data-y', j);
+        
+        if(matrix[i][j][1] == 0) {
+          // Если это не играбельная клетка.
+          cell.className = 'neutral';          
+        } else {
+          // Если это обычная клетка.
+          // Привязываем вызов функции к клетке.
+          cell.setAttribute('onclick', 'userTurn(this.dataset.x, this.dataset.y)');
+          // Раскрашиваем клетку.
+          // Цвета можно вынести в css, а тут просто раздавать классы.
+          cell.style.backgroundColor = COLORS[matrix[i][j][0]];
+          
+          // По умолчанию клетка свободная.
+          //cell.className = 'free';
 
-				cell.innerText = arr[i][j];
-				row.appendChild(cell);
-			}
-			DOM_table.appendChild(row);
-		}
-	}
+          if (matrix[i][j][1] == 1) {
+            // Выделение клеток из областей игрока.
+            cell.className = 'player';
+          } else if (matrix[i][j][1] == 2) {
+            // Выделение клеток из областей противника.
+            cell.className = 'enemy';
+          } else if (matrix[i][j][1] == 3) {
+            // Выделение свободных клеток
+            cell.className = 'free';
+          }
 
-	changeTableCellColor(x, y, color) {
-		var line = document.querySelectorAll(`[data-x='${x}']`);
-		var cell;
+          // Выделение клеток из областей игрока.
+          /*this.player_area.forEach(element => {
+            if (element[0] == i && element[1] == j){
+              cell.className = 'player';
+            }
+          });
 
-		line.forEach(element => {
-			if (element.getAttribute("data-y") == y)
-				cell = element;
-		});
-		
-		cell.style.backgroundColor = COLORS[color];
-		cell.innerText = color;
+          // Выделение клеток из областей противника.
+          this.enemy_area.forEach(element => {
+            if (element[0] == i && element[1] == j){
+              cell.className = 'enemy';
+            }
+          });*/
+        }
 
-		/*
+        //cell.innerText = matrix[i][j][0];
+        cell.innerText = i + ' ' + j;
+        row.appendChild(cell);
+      }
+      DOM_table.appendChild(row);
 
-		var line = document.querySelectorAll(`[data-x="${x}"]`);
-		console.log(line);
-		var cell = line[0].querySelector(`[data-y="${y}"]`);
-		*/
-		//console.log('[' + x + ';' + y + '] cl:' + color);
-		//this.data_object.grid[x][y] = color;
+    }
+  }
 
+  changeTableCellColor(x, y, color) {
+    var line = document.querySelectorAll(`[data-x='${x}']`);
+    var cell;
 
-		//console.log(this.data_object);
-	}
+    line.forEach(element => {
+      if (element.getAttribute("data-y") == y)
+        cell = element;
+    });
+    
+    cell.style.backgroundColor = COLORS[color];
+    cell.innerText = color;
 
-	changeAreaColor(x, y, turn) {
-		var result = true;
+    /*
 
-		if (turn == 'player') {
-			// Игрок ходит
-			// Игрок может нажать на свою область, на ничью область, на область противника, на неигровую область.
-			
-			// Проверка нажатия на свою область.
-			this.player_area.forEach(cell => {
-				if (cell[0] == x && cell[1] == y) {
-					// Значит ткнул на себя.
-					result = false;
-				}
-			});
-
-			// Проверка нажатия на область противника.
-			// Спорная тема. Нужно обкатать на рабочей игре.
-			/*this.enemy_area.forEach(cell => {
-				if (cell[0] == x && cell[1] == y) {
-					// Значит ткнул на противника.
-					result = false;
-				}
-			});*/
-
-			// Проверка нажатия на неигровую область не требуется, т.к. там нажатие невозможно.
-
-			if (result) {
-				// Получаем цвет ячейки на которую нажал игрок.
-				var new_color = this.grid[x][y];
-				
-				// Цикл по массиву ячеек игрока.
-				this.player_area.forEach(cell => {
-					// Изменяем содержимое матрицы.
-					this.changeTableCellColor(cell[0], cell[1], new_color);
-					// Изменяем отображение матрицы в таблице.
-					this.grid[cell[0]][cell[1]] = new_color;
-					
-					// Если соседняя клетка нейтральная
-					this.player_area.forEach(element => {
-						if (element != cell) {
-							console.log('lol');
-						}
-					});
-					
-					/*
-					if () {
-
-					}
-					if (this.grid[cell[0] + 1][cell[1]] == new_color && this.player_area[cell[0] + 1][cell[1]] == undefined){
-						
-					}*/
-				});
+    var line = document.querySelectorAll(`[data-x="${x}"]`);
+    console.log(line);
+    var cell = line[0].querySelector(`[data-y="${y}"]`);
+    */
+    //console.log('[' + x + ';' + y + '] cl:' + color);
+    //this.data_object.grid[x][y] = color;
 
 
-			}
-	
-		} else if (turn == 'enemy') {
-			// Противник ходит
-		}
+    //console.log(this.data_object);
+  }
 
-		return result;
-	}
+  changeAreaColor(x, y, turn) {
+    var result = true;
 
-	getTableData() {
-		var object = {
-			grid: this.grid,
-			player_area: this.player_area,
-			enemy_area: this.enemy_area,
-			player_color: this.player_color,
-			enemy_color: this.enemy_color,
-		}
-		return object;
-	}
+    if (turn == 'player') {
+      // Игрок ходит
+      // Игрок может нажать на свою область, на ничью область, на область противника, на неигровую область.
+      
+      // Проверка нажатия на свою область.
+      if (this.grid[x][y][1] == 1) {
+          // Значит ткнул на себя.
+          console.log('warning: ' + 'ткнул на себя');
+          result = false;
+      }
+
+      // Проверка нажатия на область противника.
+      // Спорная тема. Нужно обкатать на рабочей игре.
+      /*this.enemy_area.forEach(cell => {
+        if (cell[0] == x && cell[1] == y) {
+          // Значит ткнул на противника.
+          result = false;
+        }
+      });*/
+
+      // Проверка нажатия на неигровую область не требуется, т.к. там нажатие невозможно.
+
+      if (result) {
+        // Получаем цвет ячейки на которую нажал игрок.
+        var new_color = this.grid[x][y][0];
+        if (new_color == this.player_color) {
+          console.log('warning: ' + 'нажал на свой цвет');
+          result = false;
+        }
+      }
+
+      if (result) {
+        // Выбираем все ячейки игрока.
+        var player_cells = [];
+        for (var i = 0; i < this.grid.length; i++) {
+          for (var j = 0; j < this.grid[i].length; j++) {
+            if (this.grid[i][j][1] == 1)
+              player_cells.push([i, j]);
+          }
+        }
+
+        var free_colored_cells = [];
+
+        // Проходим по всем клеткам игрока и выбираем свободные смежные клетки.
+        // Смежной считается клетка имеющая с клеткой игрока общую грань.
+        // Найденные клетки добавляем в конец массива чтобы выполнить обход и для них.
+        // Цикл for...of подходит для перебора массива длинна которого увеличивается во время выполнения цикла.
+        var counter = 0;
+        for (var cell of player_cells) {
+          // Противорекурсин. Одна заглушка.
+          counter++;
+          if (counter > 50)
+            return 0;
+
+          var i = cell[0];
+          var j = cell[1];
+          var width = this.grid.length;
+          var height = this.grid[0].length;
+          console.log(cell);
+
+          if (i+1 >= 0 && i+1 < width && j >= 0 && j < height && this.grid[i+1][j][0] == new_color && this.grid[i+1][j][1] == 3) {
+            //console.log('bottom');
+            // Если элемент уникальный - добавляем.
+            if (!this.isContained(player_cells, [i+1, j]))
+              player_cells.push([i+1, j]);
+            free_colored_cells.push([i+1, j]);
+          }
+          if (i-1 >= 0 && i-1 < width && j >= 0 && j < height && this.grid[i-1][j][0] == new_color && this.grid[i-1][j][1] == 3) {
+            //console.log('top');
+            // Если элемент уникальный - добавляем.
+            if (!this.isContained(player_cells, [i-1, j]))
+              player_cells.push([i-1, j]);
+            free_colored_cells.push([i-1, j]);
+          }
+          if (i >= 0 && i < width && j+1 >= 0 && j+1 < height && this.grid[i][j+1][0] == new_color && this.grid[i][j+1][1] == 3) {
+            //console.log('right');
+            // Если элемент уникальный - добавляем.
+            if (!this.isContained(player_cells, [i, j+1]))
+              player_cells.push([i, j+1]);
+            free_colored_cells.push([i, j+1]);
+          }
+          if (i >= 0 && i < width && j-1 >= 0 && j-1 < height && this.grid[i][j-1][0] == new_color && this.grid[i][j-1][1] == 3) {
+            //console.log('left');
+            // Если элемент уникальный - добавляем.
+            if (!this.isContained(player_cells, [i, j-1]))
+              player_cells.push([i, j-1]);
+            free_colored_cells.push([i, j-1]);
+          }
+        }
+
+        // Удаляем повторяющиеся элементы из массива.
+        free_colored_cells = this.uniqueArray(free_colored_cells);
+        console.log(free_colored_cells);
+        
+        if (free_colored_cells.length == 0) {
+          console.log('warning: ' + 'нет подходящих клеток для захвата');
+          result = false;
+        }
+      }
+
+      // Собственно перекраска и присвоение области 
+      if (result) {
+
+        free_colored_cells.forEach(cell => {
+          var i = cell[0];
+          var j = cell[1];
+          this.grid[i][j][1] = 1;
+        });
+
+        player_cells.forEach(cell => {
+          var i = cell[0];
+          var j = cell[1];
+          this.grid[i][j][0] = new_color;
+        });
+
+        this.player_color = new_color;
+        this.generateTable();
+      }
+  
+    } else if (turn == 'enemy') {
+      // Противник ходит
+    }
+
+    return result;
+  }
+
+  // Возвращает все данные таблицы.
+  getTableData() {
+    var object = {
+      grid: this.grid,
+      player_area: this.player_area,
+      enemy_area: this.enemy_area,
+      player_color: this.player_color,
+      enemy_color: this.enemy_color,
+    }
+    return object;
+  }
+
+  // Возвращает массив без повторяющихся элементов.
+  uniqueArray (arr) {
+    let res = [];
+    for (let a of arr) {
+      var find = false;
+
+      for (let b of res)
+        if (a[0] == b[0] && a[1] == b[1])
+          find = true;
+      
+      if (!find)
+        res.push(a);
+    }  
+    return res;
+  }
+
+  // Проверка на содержание подмассива в массиве.
+  isContained (arr, obj) {
+    // Если arr содержит obj - возвращает true, иначе - false.
+    var res = false;
+    arr.forEach(a => {
+      if (a[0] == obj[0] && a[1] == obj[1])
+        res = true;
+    });
+    return res;
+  }
+
+  // Удаляет всех потомков DOM элемента.
+  removeAllChildNodes (parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+  }
 }
